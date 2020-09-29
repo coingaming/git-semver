acb43a206e3ee5f1d7153444ee0ce8a24dcde573
Ilja Tkachuk <tkachuk.labs@gmail.com>
2020-09-29 16:32:37 +0300

    nixify

---
+++ default.nix
@@ -0,0 +1,18 @@
+let nixpkgs = import ./nixpkgs19.nix;
+in
+{
+  pkgs ? import nixpkgs {}
+}:
+with pkgs;
+
+stdenv.mkDerivation {
+  name = "git-semver";
+  src = ./.;
+  dontBuild = true;
+  installPhase = ''
+    mkdir -p $out/bin/
+    cp ./git-semver.sh $out/bin/git-semver.sh
+    wrapProgram "$out/bin/git-semver.sh" \
+      --set SYSTEM_CERTIFICATE_PATH "${cacert}/etc/ssl/certs"
+  '';
+}
